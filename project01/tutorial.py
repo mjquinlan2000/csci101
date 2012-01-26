b4195fcc9523a7b9ca113f9483239d9454384712
Mike Quinlan <mquinlan@mines.edu>
2012-01-25 21:18:53 -0700

    adding tutorial stuff

---
+++ project01/tutorial.py
@@ -0,0 +1,8 @@
+print 'Name: Michael Quinlan'
+print 'Birthday: 14th March 1990'
+print '2 * 5 is ', 2*5
+print '2^23 is ', 2**23
+print '2/3 is ', 2/3
+print "Wait, that's not right... 2/3 is acutally", 2/3.0
+print '77 - 34 is ', 77 - 34
+print '67 * 23 is ', 67*23
