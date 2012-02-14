686a1059debbfe915ade98fb90d8e070ba9b11bb
Mike Quinlan <mquinlan@mines.edu>
2012-02-14 13:47:24 -0700

    adding final course grade python script

--- course_grade/CourseGrade.py
+++ course_grade/CourseGrade.py
@@ -5,10 +5,7 @@ def check_score(score):
 
 name = raw_input("What is your name?\n")
 
-titles = ['Final Exam', 'Exam I', 'Exam II', 'Programming and Written Assignments', 'Quizzes', 'Group score on Quizzes']
-
 total_score = 0
-scores = dict()
 weights = dict()
 
 weights['Final Exam'] = 25
@@ -16,14 +13,13 @@ weights['Exam I'] = 15
 weights['Exam II'] = 20
 weights['Programming and Written Assignments'] = 16
 weights['Quizzes'] = 12
-weights['Group score on Quizzes'] = 12
+weights['Median group score on Quizzes'] = 12
 
-for title in titles:
+for title in weights.keys():
 	score = -1
 	while check_score(score) == False:
 		score = input("What is your " + title + " Grade?\n")
-	scores[title] = score
 	
 	total_score += (score/100.0)*weights[title]
 
-print str(total_score) + " Percent"
+print name + ", your current course grade is", str(total_score) + "%"
