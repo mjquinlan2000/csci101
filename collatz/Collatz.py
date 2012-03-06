d1a2b57c6c91edbe9d312201d31d4c769b2a994b
Mike Quinlan <mquinlan@mines.edu>
2012-03-06 00:41:00 -0700

    fixed collatz

--- collatz/Collatz.py
+++ collatz/Collatz.py
@@ -7,21 +7,21 @@ increases = 0
 decreases = 0
 
 #Recursive Hailstone Sequence function
-def hailstone_seq(n, outstr):
+def hailstone_seq(n):
 	global increases
 	global decreases
 	if n == 1:
-		outstr += str(n)
-		return outstr
+		print str(n)
+		return
 	
 	if n % 2 == 0:
-		outstr += str(n) + " "
+		print str(n),
 		increases += 1
-		return hailstone_seq(n/2, outstr)
+		return hailstone_seq(n/2)
 	else:
-		outstr += str(n) + " "
+		print str(n),
 		decreases += 1
-		return hailstone_seq(3*n + 1, outstr)
+		return hailstone_seq(3*n + 1)
 
 #Input helper function
 def check_input(n):
@@ -43,6 +43,5 @@ while not check_input(uinput):
 
 uinput = int(uinput)
 
-outstr = hailstone_seq(uinput, "")
-print outstr
+hailstone_seq(uinput)
 print "Hailstone sequence lenth for {0:d} is {1:d} with {2:d} increases and {3:d} decreases".format(uinput, increases + decreases + 1, increases, decreases)
