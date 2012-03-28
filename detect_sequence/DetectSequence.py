ff264e3f90c3546fc2baa7126dd845cbfe8895ec
Mike Quinlan <mquinlan@mines.edu>
2012-03-28 17:37:13 -0600

    adding more stuff

--- detect_sequence/DetectSequence.py
+++ detect_sequence/DetectSequence.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
 #Created By: Mike Quinlan
 
+lines = list()
+nums = list()
+
 with open("sequence.txt") as file:
 	lines = file.readlines()
 
 for line in lines:
-	line = int(line)
-	print line
+	try:
+		nums.append(int(line))
+	except ValueError:
+		print "NOT A VALID INPUT: " + line.rstrip('\n')
+		exit
+
+def detect_sequence(a, eps, numbers):
+	if numbers.count(a) < 2:
+		print "BOGUS"
+		exit
+	
+	first_a = numbers.index(a)
+	for i in range(first_a):
+		numbers.pop(0)
+	print numbers
+
+detect_sequence(3, 7, nums)
