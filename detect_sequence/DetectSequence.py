2dae893fcdc3e8cab9208bd588350fdddc81a195
Mike Quinlan <mquinlan@mines.edu>
2012-04-02 16:30:09 -0600

    final commit for detect_sequence

--- detect_sequence/DetectSequence.py
+++ detect_sequence/DetectSequence.py
@@ -15,9 +15,10 @@ for line in lines:
 		exit
 
 def detect_sequence(a, eps, numbers):
-	is_sequence = False
+	double_eps = False
+	seq_length = 0
 	
-	if numbers.count(a) < 2 or len(numbers) == 0:
+	if numbers.count(a) < 2:
 		print "BOGUS"
 		exit
 	
@@ -25,14 +26,25 @@ def detect_sequence(a, eps, numbers):
 	for i in range(first_a):
 		numbers.pop(0)
 	
-	while not is_sequence and len(numbers) > 0:
-		if numbers[0] == a:
-			print "Something"
-		elif numbers[0] == eps:
-			if numbers[1] == a:
-				numbers.pop(0)
-		else:
-			print "One last thing"
-		numbers.pop(0)
+	list_length = len(numbers)
+	for i in range(list_length):
+		
+		seq_length = seq_length + 1
+		if(numbers[i] == a) and i != 0:
+			if numbers[i-1] != eps or double_eps == True:
+				print "Accept", seq_length
+				break
+		
+		if(numbers[i] == a) and i == 1:
+			print "Accept", seq_length
+			break
+		
+		double_eps = False
+		
+		if numbers[i] == eps and numbers[i-1] == eps:
+			double_eps = True
+		
+		if i == list_length - 1:
+			print "BOGUS"
 
 detect_sequence(3, 7, nums)
