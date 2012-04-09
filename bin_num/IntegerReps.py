b4736426f64452fcb6f5f1cd34daf47700f1d488
Mike Quinlan <mquinlan@mines.edu>
2012-04-09 11:33:54 -0600

    working decimal to binary function added

--- bin_num/IntegerReps.py
+++ bin_num/IntegerReps.py
@@ -5,16 +5,22 @@
 
 def decimal_value(bin_list, outval, base):
 	if len(bin_list) == 0:
+		print outval
 		return outval
 	
 	#Assuming the value is MSB first
 	bin_list.reverse()
-	if bin_list[0] == '1':
+	if bin_list[0] == 1:
 		outval = outval + base
 	bin_list.pop(0)
 	bin_list.reverse()
-	decimal_value(bin_list, outval, base*2)
+	return decimal_value(bin_list, outval, base*2)
 	
 			
 
 def binary_bits(int_value):
+	return True
+
+bin_list = [1,0,1,1,0]
+
+x = decimal_value(bin_list, 0, 1)
