093b01b8b8fdffe64b7e82238c2f5c03157b9d3a
Mike Quinlan <mquinlan@mines.edu>
2012-04-09 11:50:44 -0600

    almost woring function

--- bin_num/IntegerReps.py
+++ bin_num/IntegerReps.py
@@ -5,7 +5,6 @@
 
 def decimal_value(bin_list, outval, base):
 	if len(bin_list) == 0:
-		print outval
 		return outval
 	
 	#Assuming the value is MSB first
@@ -18,9 +17,20 @@ def decimal_value(bin_list, outval, base):
 	
 			
 
-def binary_bits(int_value):
-	return True
+def binary_bits(int_value, out_list):
+	if int_value == 0:
+		if len(out_list) == 0:
+			return [0]
+		else:
+			return out_list.reverse()
+	print int_value, int_value%2, int_value/2
+	out_list.append(int_value % 2)
+	return binary_bits(int_value/2, out_list)
 
 bin_list = [1,0,1,1,0]
 
 x = decimal_value(bin_list, 0, 1)
+
+aList = binary_bits(x, list())
+
+print x, aList
