2832f2a8521e875ef4783d26a0079eb552173f4f
Mike Quinlan <mquinlan@mines.edu>
2012-04-17 18:30:07 -0600

    Adding final studen.py

--- checkerboard/student.py
+++ checkerboard/student.py
@@ -6,13 +6,20 @@ from main import placetile, divideboard, Main
 #
 # Place your answer here, as a Python comment (all lines preceded with a #)
 # 
+# The relative function places an L in the square that doesn't have a taken
+# piece so that there is a taken piece in all other squares then it calls
+# checkerboard on the square in which it originally found the missing piece
+# when it gets to the 2x2 square, it places the L in the only place it will fit
 #
-#
-#
+# The clockwise function places an L in the square that doesn't have a taken
+# piece so that there is a taken piece in all other squares. Then it calls
+# checkerboard recursively on the upper left quadrant. When it gets to the 2x2
+# piece in the top left, it works over all other larger quadrants in a counter-
+# clockwise fashion
 #
 #################################################################################
 
-def checkerboard( cb ) :
+def cbOriginal( cb ) :
     if len( cb ) == 2 :
         ###
         # placetile draws a newly colored L shaped tile over the 2x2 checkerboard
@@ -35,10 +42,10 @@ def checkerboard( cb ) :
         # love to say more, but we would be giving away Q1.
         ###
         m, n, p, q = divideboard( cb, 'clockwise' )
-        checkerboard( m )
-        checkerboard( q )
-        checkerboard( p )
-        checkerboard( n )
+        cbOriginal( m )
+        cbOriginal( q )
+        cbOriginal( p )
+        cbOriginal( n )
 
 
 #################################################################################
@@ -52,7 +59,34 @@ def checkerboard( cb ) :
 
 
 
-
+def A( cb ) :
+    if len( cb ) == 2 :
+        ###
+        # placetile draws a newly colored L shaped tile over the 2x2 checkerboard
+        # (the cb argument).
+        #
+        # placetile will fail if cb is not 2x2, or if cb doesn't have exactly one
+        # square that is either covered by a previous tile or the missing square
+        # in the puzzle setup.  If placetile fails, it will display an error
+        # message saying so.
+        ###
+        placetile(cb)
+    else :
+        ###
+        # divideboard takes a checkerboard with 2**(2**n), n>0, squares and 
+        # chops it up into 4 equal quadrants.  The quadrants are returned as 4
+        # values, and stored here into m, n, p, and q.
+        #
+        # The specific order and relationships between m, n, p, and q are dictated
+        # by whether the second argument is 'clockwise' or 'relative'.  We'd 
+        # love to say more, but we would be giving away Q1.
+        ###
+        m, n, p, q = divideboard( cb, 'clockwise' )
+        A( m )
+        A( n )
+        A( q )
+        A( p )
+        
 
 
 
@@ -65,7 +99,33 @@ def checkerboard( cb ) :
 # cbOriginal or A) so that it reproduces the recursive pattern for Challenge B.
 #################################################################################
 
-
+def B( cb ) :
+    if len( cb ) == 2 :
+        ###
+        # placetile draws a newly colored L shaped tile over the 2x2 checkerboard
+        # (the cb argument).
+        #
+        # placetile will fail if cb is not 2x2, or if cb doesn't have exactly one
+        # square that is either covered by a previous tile or the missing square
+        # in the puzzle setup.  If placetile fails, it will display an error
+        # message saying so.
+        ###
+        placetile(cb)
+    else :
+        ###
+        # divideboard takes a checkerboard with 2**(2**n), n>0, squares and 
+        # chops it up into 4 equal quadrants.  The quadrants are returned as 4
+        # values, and stored here into m, n, p, and q.
+        #
+        # The specific order and relationships between m, n, p, and q are dictated
+        # by whether the second argument is 'clockwise' or 'relative'.  We'd 
+        # love to say more, but we would be giving away Q1.
+        ###
+        m, n, p, q = divideboard( cb, 'relative' )
+        B( m )
+        B( p )
+        B( q )
+        B( n )
 
 
 
@@ -83,9 +143,32 @@ def checkerboard( cb ) :
 
 
 
-
-
-
+def C( cb ) :
+    if len( cb ) == 2 :
+        ###
+        # placetile draws a newly colored L shaped tile over the 2x2 checkerboard
+        # (the cb argument).
+        #
+        # placetile will fail if cb is not 2x2, or if cb doesn't have exactly one
+        # square that is either covered by a previous tile or the missing square
+        # in the puzzle setup.  If placetile fails, it will display an error
+        # message saying so.
+        ###
+        placetile(cb)
+    else :
+        ###
+        # divideboard takes a checkerboard with 2**(2**n), n>0, squares and 
+        # chops it up into 4 equal quadrants.  The quadrants are returned as 4
+        # values, and stored here into m, n, p, and q.
+        #
+        # The specific order and relationships between m, n, p, and q are dictated
+        # by whether the second argument is 'clockwise' or 'relative'.  We'd 
+        # love to say more, but we would be giving away Q1.
+        ###
+        m, n, p, q = divideboard( cb, 'clockwise' )
+        C( m )
+        C( p )
+        C( q )
 
 
 
